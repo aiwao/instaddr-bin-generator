0a525f16744f647609da2949cc32b70a0026a231
aiwao <233898056+aiwao@users.noreply.github.com>
2026-01-18 16:29:34 +0900

    fix sql syntax error

--- init.sql
+++ init.sql
@@ -2,5 +2,5 @@ CREATE TABLE IF NOT EXISTS accounts (
     id TEXT NOT NULL,
     password TEXT NOT NULL,
     amount INTEGER NOT NULL,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now()
 );
